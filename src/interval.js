6c02fa4fb3cb9707d377534fabdc11e325313cc5
highmountaintea <jlpub@coolimage.com>
2019-01-08 15:57:15 -0500

    created interval() function

---
+++ src/interval.js
@@ -0,0 +1,94 @@
+/**
+ * runs the designated function periodically. This utility will wait for one invocation to
+ * finish before it runs the next in line. It always waits for ms amount of time between the end
+ * of one invocation and the beginning of the next
+ * @param {*} fn function to invoke every interval
+ * @param {*} ms invocation interval in milliseconds
+ * @param {*} options not implemented yet
+ */
+export function interval(fn, ms, options) { // eslint-disable-line no-unused-vars
+  if (ms == null || typeof ms !== 'number') throw new Error('invalid ms');
+
+  const settings = {
+    ms: ms,
+    halted: false,
+  };
+
+  function wait(newms) {
+    try {
+      if (newms != null && typeof newms === 'number') settings.nextMs = newms;
+    } catch (e) {
+      // swallow
+    }
+  }
+
+  function newInterval(newms) {
+    try {
+      if (newms != null && typeof newms === 'number') settings.ms = newms;
+    } catch (e) {
+      // swallow
+    }
+  }
+
+  function pause() {
+  }
+
+  function resume() {
+  }
+
+  function halt() {
+    settings.halted = true;
+  }
+
+  // async function thread() {
+  //   try {
+  //     const control = { wait, newInterval, pause, resume, halt };
+  //     const result = fn(control);
+  //     if (result != null && result.then != null) {
+  //       await result;
+  //     }
+  //   } catch (e) {
+  //     // swallow, may invoke error handler in the future when options is implemented
+  //   } finally {
+  //     let nextMs;
+  //     if (settings.nextMs != null) {
+  //       nextMs = settings.nextMs;
+  //       delete settings.nextMs;
+  //     } else {
+  //       nextMs = settings.ms;
+  //     }
+  //     if (!settings.halted) setTimeout(thread, nextMs);
+  //   }
+  // }
+
+  function thread() {
+    function queueNext() {
+      // stop if halted is set
+      if (settings.halted) return;
+
+      // figure out how long to wait
+      let nextMs;
+      if (settings.nextMs != null) {
+        nextMs = settings.nextMs;
+        delete settings.nextMs;
+      } else {
+        nextMs = settings.ms;
+      }
+
+      // call
+      setTimeout(thread, nextMs);
+    }
+
+    let result;
+    try {
+      const control = { wait, newInterval, pause, resume, halt };
+      result = fn(control);
+    } catch (e) {
+      // swallow, may invoke error handler in the future when options is implemented
+    }
+    Promise.resolve(result).finally(queueNext);
+  }
+
+
+  setTimeout(thread, settings.ms);
+}
---
+++ test/test2.js
@@ -0,0 +1,10 @@
+const jugger = require('../lib');
+
+jugger.interval(async (control) => {
+  let num = Math.random();
+  console.log(num);
+  if (num < 0.05) control.halt();
+  else if (num < 0.10) control.newInterval(3000);
+  else if (num < 0.20) control.wait(3000);
+  else if (num > 0.90) control.newInterval(1000);
+}, 1000);
